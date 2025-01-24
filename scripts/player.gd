4c7cefd867ce329497ed796e4decc1ed7e12efbb
gamejam005 <gamejam005@student.sit.ac.nz>
2025-01-25 12:20:06 +1300

    added swim controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,16 +4,22 @@ class_name Player
 
 @export var walk_speed := 300.0
 @export var jump_velocity := 400.0
+@export var swim_speed := 100.0
 
 @onready var ground_controller: Node = $GroundController
 @onready var water_controller: Node = $WaterController
 
-var active_controller: Node
+
+var active_controller: Node:
+	set(value):
+		motion_mode = (MOTION_MODE_GROUNDED if value == ground_controller
+					   else MOTION_MODE_FLOATING)
+		active_controller = value
 
 
 func _ready() -> void:
 	# assume we start game on the ground
-	active_controller = ground_controller
+	active_controller = water_controller
 	
 	# give controllers access to player
 	for controller in [ground_controller, water_controller]:
---
+++ scripts/water_controller.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var player: Player
+
+
+func update(delta: float) -> void:
+	# Add the gravity.
+	if not player.is_on_floor():
+		player.velocity += player.get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if direction:
+		player.velocity = direction * player.swim_speed
+	else:
+		player.velocity = Vector2.ZERO
+
+	player.move_and_slide()
