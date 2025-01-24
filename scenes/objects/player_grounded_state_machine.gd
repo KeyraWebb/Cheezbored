cd0a97da13ec55d3932f45ae2627a010667af61f
gamejam005 <gamejam005@student.sit.ac.nz>
2025-01-24 21:07:10 +1300

    start controller and state machine

---
+++ scenes/objects/player_grounded_state_machine.gd
@@ -0,0 +1,9 @@
+extends StateMachine
+
+func ready() -> void:
+	super()
+	pass # Replace with function body.
+
+
+func process(delta: float) -> void:
+	pass
---
+++ scripts/state_machine.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name StateMachine
+
+
+# Not actually the ready function used by Godot
+# this gets called from whatever is using the state machine
+func ready() -> void:
+	pass # Replace with function body.
+
+
+func process(delta: float) -> void:
+	pass
