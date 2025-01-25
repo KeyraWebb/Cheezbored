2b1f60684f2e1daec06c16bd9309f9f7b8a84c9e
otherotherjosh <itsotherjosh@gmail.com>
2025-01-26 11:13:48 +1300

    respawn

--- scripts/beacon.gd
+++ scripts/beacon.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var pipes: Node2D = $Pipes
+@onready var oxygen_timer: OxygenTimer = %OxygenTimer
+
+signal set_respawn(respawn_position: Vector2)
 
 
 func _ready() -> void:
@@ -11,3 +14,5 @@ func _ready() -> void:
 func on_beacon_placed() -> void:
 	pipes.process_mode = Node.PROCESS_MODE_INHERIT
 	pipes.show()
+	oxygen_timer.oxygen = 100
+	set_respawn.emit(global_position)
