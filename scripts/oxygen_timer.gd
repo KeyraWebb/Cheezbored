516fad9bd331182d7a3ea9e9ac43e8781311639a
KeyraWebb <keyrajrwebb@gmail.com>
2025-01-25 15:54:39 +1300

    oxygen timer goes red below 20%

--- scripts/oxygen_timer.gd
+++ scripts/oxygen_timer.gd
@@ -4,6 +4,7 @@ var oxygen = 100
 @export var deplete_delay: int
 var deplete_timer = 0
 @export var oxygen_display: Label
+@export var low_oxygen_color: Color
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,6 +18,9 @@ func _process(delta: float) -> void:
 		deplete_timer = 0
 	else:
 		deplete_timer += 1
+		
+	if oxygen < 20:
+		oxygen_display.set("theme_override_colors/font_color", low_oxygen_color)
 	
 	oxygen_display.text = "Oxygen: " + str(oxygen) + "%"
 	pass
