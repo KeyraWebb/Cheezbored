1df1edf0bdd27a3be3365381befa36bead8c3363
otherotherjosh <itsotherjosh@gmail.com>
2025-01-26 03:08:03 +1300

    fix text existing too early

--- scripts/beacon_area.gd
+++ scripts/beacon_area.gd
@@ -8,7 +8,8 @@ signal on_beacon_placed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	tooltip.hide_tooltip()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
