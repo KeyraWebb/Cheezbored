ba02e0f7117116727985fd5bd1df8e29e28052d2
otherotherjosh <itsotherjosh@gmail.com>
2025-01-26 01:14:46 +1300

    oxygen dispenser checks player collision

--- scripts/oxygen_timer.gd
+++ scripts/oxygen_timer.gd
@@ -4,8 +4,10 @@ extends Control
 @export var oxygen_display: Label
 @export var low_oxygen_color: Color
 
-var oxygen = 100
 var deplete_timer = 0
+var oxygen = 100
+
+signal on_depleted
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,22 +16,23 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if deplete_timer >= deplete_delay:
 		oxygen -= 1
 		deplete_timer = 0
-	elif oxygen > 0:
+	elif oxygen >= 0:
 		deplete_timer += 1
 		
 	if oxygen < 20:
 		oxygen_display.set("theme_override_colors/font_color", low_oxygen_color)
 	
-	if oxygen <= 0:
+	oxygen_display.text = "Oxygen: " + str(oxygen) + "%"
+	
+	if oxygen < 0:
 		oxygen_depleted()
 	
-	oxygen_display.text = "Oxygen: " + str(oxygen) + "%"
 
 #for when oxygen runs out
 func oxygen_depleted():
-	pass
+	oxygen_display.text = "Oxygen: N/A"
 	
